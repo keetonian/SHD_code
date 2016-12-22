79268b96576456c9e57078ad8c2695a854319fdf
Keeton Hodgson <keeton.hodgson@gmail.com>
2016-12-22 16:06:13 -0700

    Cleaned up crossbar-shd code

--- Crossbar16/crossbar_shd.cpp
+++ Crossbar16/crossbar_shd.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 #include <stdlib.h>
 #include <fstream>
+#include <string>
+#include <sstream>
 
-#include "compare16.cpp"
 #include "ctpl.h"
-#include "compare4.cpp"
 
 using namespace std;
 
@@ -15,14 +15,15 @@ void PrepareReference16(char* ref_file, vector<unsigned short> * ref);
 void PrepareReference4(char* ref_file, vector<unsigned char> * ref);
 unsigned short ConvertCharacters16(char char1, char char2);
 unsigned short ConvertInverseCharacters16(char char1, char char2);
-//static std::vector<unsigned short> * ref = new vector<unsigned short>(0);
 void read_compare_func16(int id, string header, string read_line, vector<unsigned short> * ref, int shift, int threshold);
 void read_compare_func4(int id, string header, string read_line, vector<unsigned char> * ref, int shift, int threshold);
-//static std::vector<unsigned short> * read = new vector<unsigned short>(0);
 void CompareRead16(char* read_file, char* reference_file, int shift, int threshold);
 void CompareRead4(char* read_file, char* reference_file, int shift, int threshold);
 unsigned char ConvertCharacter4(char char1);
 unsigned char ConvertCharacterInverse4(char char1);
+
+string compare16(vector<unsigned short>* ref, vector<unsigned short> read_in, vector<unsigned short> inverse, int shift, int threshold, int* num_matches);
+string compare4(vector<unsigned char>* ref, vector<unsigned char> read_in, vector<unsigned char> inverse, int shift, int threshold, int* num_matches);
 
 static int report_total_matches = 0;
 
@@ -128,8 +129,6 @@ int main(int argc, char** argv)
    *    Accurately returns the number of matches.
    */
 
-  // Not used yet.
-  vector<long> chromosomes();
   if(encoding){
     cout<<"16 Bit encodings.\n";
     CompareRead16(read_file, reference_file, shift, threshold);
@@ -164,6 +163,10 @@ char* ParseFile(char** argv, int i) {
   return file_name;
 }
 
+/*
+ * Compares each read in the read_file to the reference genome according to the shift and threshold
+ * 16 bit version
+ */
 void CompareRead16(char* read_file, char* reference_file, int shift, int threshold) {
   std::vector<unsigned short> ref(0);
   ref.reserve(3500000000);
@@ -199,6 +202,9 @@ void CompareRead16(char* read_file, char* reference_file, int shift, int thresho
   }
 } 
 
+/*
+ * Helper function for CompareRead16. This function is passed into thread arguments
+ */
 void read_compare_func16(int id, string header, string read_line, vector<unsigned short> * ref, int shift, int threshold) {
   int j = 0;
   // Save read sequence
@@ -216,11 +222,15 @@ void read_compare_func16(int id, string header, string read_line, vector<unsigne
   header += compare16(ref, readv, read_inverse, shift, threshold, &num_matches);
 
   if(report_total_matches)
-    cout<<header<<"TOTAL MATCHES: " << num_matches << endl;
+    cout<<header<<"TOTAL MATCHES: " << num_matches << '\n' << endl;
   else
     cout<<header<<endl;
 }
 
+/*
+ * Compares each read in the read_file to the reference genome according o the shift and threshold
+ * 4 bit version
+ */
 void CompareRead4(char* read_file, char* reference_file, int shift, int threshold) {
   std::vector<unsigned char> ref(0);
   ref.reserve(3500000000);
@@ -256,6 +266,9 @@ void CompareRead4(char* read_file, char* reference_file, int shift, int threshol
   }
 } 
 
+/*
+ * Helper function for CompareRead4. THis function is passed into threads to be run
+ */
 void read_compare_func4(int id, string header, string read_line, vector<unsigned char> * ref, int shift, int threshold) {
   int j = 0;
   // Save read sequence
@@ -273,7 +286,7 @@ void read_compare_func4(int id, string header, string read_line, vector<unsigned
   header += compare4(ref, readv, read_inverse, shift, threshold, &num_matches);
 
   if(report_total_matches)
-    cout<<header<<"TOTAL MATCHES: " << num_matches << endl;
+    cout<<header<<"TOTAL MATCHES: " << num_matches << '\n' << endl;
   else
     cout<<header<<endl;
 }
@@ -417,12 +430,14 @@ unsigned short ConvertInverseCharacters16(char char1, char char2) {
     return 0;
 }
 
-
+/*
+ * Converts the reference into 4 bit codes
+ */
 void PrepareReference4(char* ref_file, vector<unsigned char> * ref){
   string line;
-  cout << "preparing reference" << endl;
   ifstream reference(ref_file);
   if(reference.is_open()) {
+
     // What if we tried buffers instead of lines?
     while(getline(reference, line)) {
       if(line.size() ==0)
@@ -444,7 +459,6 @@ void PrepareReference4(char* ref_file, vector<unsigned char> * ref){
     cerr << "Unable to open reference file\n";
     exit(1);
   }
-  cout << "reference prepared" << endl;
 }
 
 
@@ -472,3 +486,246 @@ unsigned char ConvertCharacterInverse4(char char1) {
   }
 }
 
+
+
+/**
+ * Function to do the 16-bit comparison.
+ * ref: pointer to reference genome (large compared to read). Format: 16-bit 1-hot encodings.
+ * read: pointer to read sequence. Again, 16-bit 1-hot encodings.
+ * shift: multiplex distance
+ * threshold: minimum value allowed.
+ */
+string compare16(vector<unsigned short>* ref, vector<unsigned short> read_in, vector<unsigned short> inverse, int shift, int threshold, int* num_matches) {
+
+  // Set up space for results.
+  stringstream s;
+  int matches = 0;
+
+
+  // Set up variables
+  unsigned int i = 0;
+  unsigned int k = 0;
+  unsigned int read_size = read_in.size();
+  vector<unsigned short> read(0);
+  vector<unsigned short> inv(0);
+  read.reserve(read_size);
+  inv.reserve(read_size);
+  unsigned int ref_size = ref->size();
+
+  for(i=0; i < read_size; i++) {
+    // Prepare the read parameters
+    unsigned short a = read_in.at(i);
+    unsigned short b = inverse.at(i);
+    unsigned int j;
+    for(j=0; j <= (unsigned)shift; j++) {
+      if(i >= j){
+        a = a | read_in.at(i-j);
+        b = b | inverse.at(i-j);
+      }
+      if((unsigned)(i+j)<read_size){
+        a = a | read_in.at(i+j);
+        b = b | inverse.at(i+j);
+      }
+    }
+    inv.push_back(b);
+    read.push_back(a);
+  }
+
+  // Compare read strand with entire reference genome.
+  for(k = 0; k <= ref_size - read_size; k++) {
+    int error = 0;
+    int error_inv = 0;
+    for(i = 0; i < read_size; i++) {
+      // Prepare the read parameters
+      unsigned short a = read.at(i);
+      unsigned short b = inv.at(i);
+      unsigned short r = ref->at(k+i);
+
+      // Do the comparison
+      error+=(!(a&r));
+      error_inv+=(!(b&r));
+      if(error > threshold && error_inv > threshold) {
+        break;
+      }
+
+    }
+    //If the result is over or equal to the threshold
+    if(error <= threshold){
+      unsigned int kk = 0;
+      s << k << "\tn\t" << error << '\t';
+      for(; kk < read_size; kk+=2) {
+        unsigned short c1 = ref->at(k+kk);
+        char c2 = 0x20;
+        char c3 = 0x20;
+        switch(c1) {
+          case 0x8000: c2 = 'A'; c3 = 'A'; break;
+          case 0x4000: c2 = 'A'; c3 = 'T'; break;
+          case 0x2000: c2 = 'A'; c3 = 'C'; break;
+          case 0x1000: c2 = 'A'; c3 = 'G'; break;
+          case 0x0800: c2 = 'T'; c3 = 'A'; break;
+          case 0x0400: c2 = 'T'; c3 = 'T'; break;
+          case 0x0200: c2 = 'T'; c3 = 'C'; break;
+          case 0x0100: c2 = 'T'; c3 = 'G'; break;
+          case 0x0080: c2 = 'C'; c3 = 'A'; break;
+          case 0x0040: c2 = 'C'; c3 = 'T'; break;
+          case 0x0020: c2 = 'C'; c3 = 'C'; break;
+          case 0x0010: c2 = 'C'; c3 = 'G'; break;
+          case 0x0008: c2 = 'G'; c3 = 'A'; break;
+          case 0x0004: c2 = 'G'; c3 = 'T'; break;
+          case 0x0002: c2 = 'G'; c3 = 'C'; break;
+          case 0x0001: c2 = 'G'; c3 = 'G'; break;
+          default: c2 = ' '; c3 = ' '; break;
+        }
+        s << c2 << c3;
+      }
+      s<<"\n";
+      matches += 1;
+    }
+
+    if(error_inv <= threshold){
+      unsigned int kk = 0;
+      s << k << "\tri\t" << error_inv << '\t';
+      for(; kk < read_size; kk+=2) {
+        unsigned short c1 = ref->at(k+kk);
+        char c2 = 0x20;
+        char c3 = 0x20;
+        switch(c1) {
+          case 0x8000: c2 = 'A'; c3 = 'A'; break;
+          case 0x4000: c2 = 'A'; c3 = 'T'; break;
+          case 0x2000: c2 = 'A'; c3 = 'C'; break;
+          case 0x1000: c2 = 'A'; c3 = 'G'; break;
+          case 0x0800: c2 = 'T'; c3 = 'A'; break;
+          case 0x0400: c2 = 'T'; c3 = 'T'; break;
+          case 0x0200: c2 = 'T'; c3 = 'C'; break;
+          case 0x0100: c2 = 'T'; c3 = 'G'; break;
+          case 0x0080: c2 = 'C'; c3 = 'A'; break;
+          case 0x0040: c2 = 'C'; c3 = 'T'; break;
+          case 0x0020: c2 = 'C'; c3 = 'C'; break;
+          case 0x0010: c2 = 'C'; c3 = 'G'; break;
+          case 0x0008: c2 = 'G'; c3 = 'A'; break;
+          case 0x0004: c2 = 'G'; c3 = 'T'; break;
+          case 0x0002: c2 = 'G'; c3 = 'C'; break;
+          case 0x0001: c2 = 'G'; c3 = 'G'; break;
+          default: c2 = ' '; c3 = ' '; break;
+        }
+        s << c2 << c3;
+      }
+      s << "\n";
+      matches += 1;
+    }
+  }
+
+  (*num_matches) = matches;
+
+  return s.str();
+}
+
+
+/**
+ * Function to do the 4-bit comparison.
+ * ref: pointer to reference genome (large compared to read). Format: 16-bit 1-hot encodings.
+ * read: pointer to read sequence. Again, 4-bit 1-hot encodings.
+ * shift: multiplex distance
+ * threshold: minimum value allowed.
+ */
+string compare4(vector<unsigned char>* ref, vector<unsigned char> read_in, vector<unsigned char> inverse, int shift, int threshold, int* num_matches) {
+
+  // Set up space for results.
+  stringstream s;
+  int matches = 0;
+
+  // Set up variables
+  unsigned int i = 0;
+  unsigned int k = 0;
+  unsigned int read_size = read_in.size();
+  unsigned int ref_size = ref->size();
+  vector<unsigned char> read(0);
+  vector<unsigned char> inv(0);
+  inv.reserve(read_size);
+  read.reserve(read_size);
+
+  // Prepare the forward and backwards reads
+  for(i = 0; (unsigned)i < read_size; i++) {
+    unsigned char a = read_in.at(i);
+    unsigned char b = inverse.at(i);
+    unsigned int j; 
+    for(j= 0; j <= (unsigned)shift; j++) {
+      if(i >= j){
+        a = a | read_in.at(i-j);
+        b = b | inverse.at(i-j);
+      }
+      if((i+j)<read_size){
+        a = a | read_in.at(i+j);
+        b = b | inverse.at(i+j);
+      }
+    }
+    read.push_back(a);
+    inv.push_back(b);
+  }
+
+  // Compare read strand with entire reference genome.
+  for(k = 0; k <= ref_size - read_size; k++) {
+    int error = 0;
+    int error_inv = 0;
+
+    for(i = 0; i < read_size; i++) {
+      // Prepare the read parameters
+      unsigned char a = read.at(i);
+      unsigned char b = inv.at(i);
+      unsigned char r = ref->at(k+i);
+
+      // Do the comparison
+      error += (!(a&r));
+      error_inv += (!(b&r));
+      if(error > threshold && error_inv > threshold) {
+        break;
+      }
+    }
+
+    //If the result is over or equal to the threshold
+    if(error <= threshold){
+      unsigned int kk = 0;
+      s << k << "\tn\t" << error << '\t';
+      for(; kk < read_size; kk++) {
+        unsigned char c1 = ref->at(k+kk);
+        char c2 = 0x20;
+        switch(c1) {
+          case 0x8: c2 = 'A'; break;
+          case 0x4: c2 = 'T'; break;
+          case 0x2: c2 = 'C'; break;
+          case 0x1: c2 = 'G'; break;
+          case 0x0: c2 = 'N'; break;
+          default: c2 = ' '; break;
+        }
+        s << c2;
+      }
+      s << "\n";
+      matches += 1;
+    }
+
+    //If the result is over or equal to the threshold
+    if(error_inv <= threshold){
+      unsigned int kk = 0;
+      s << k << "\tn\t" << error_inv << '\t';
+      for(; kk < read_size; kk++) {
+        unsigned char c1 = ref->at(k+kk);
+        char c2 = 0x20;
+        switch(c1) {
+          case 0x8: c2 = 'A'; break;
+          case 0x4: c2 = 'T'; break;
+          case 0x2: c2 = 'C'; break;
+          case 0x1: c2 = 'G'; break;
+          case 0x0: c2 = 'N'; break;
+          default: c2 = ' '; break;
+        }
+        s << c2;
+      }
+      s << "\n";
+      matches += 1;
+    }
+  }
+
+  (*num_matches) = matches;
+
+  return s.str();
+}
